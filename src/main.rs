f3e715e43c97fd2d707463e31b2277a91fd73bed
joshrutta <joshrutta7@gmail.com>
2024-11-10 23:58:32 -0500

    integ test working w random port

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use zero2prod::run;
-
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    run(listener)?.await
 }
