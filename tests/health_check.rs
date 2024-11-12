c5d2f247fe99f35d960488fd2a7bd58fc9dbea50
joshrutta <joshrutta7@gmail.com>
2024-11-11 23:50:35 -0500

    post handler and basic post test added

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -17,6 +17,24 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    // Act
+    let body = "name=brandon%20sanderson&email=bsanderson%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
     .expect("Failed to random port");
