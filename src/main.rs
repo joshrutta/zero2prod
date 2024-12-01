347230737b62b5ac3caee98783270246ca624ca6
joshrutta <joshrutta7@gmail.com>
2024-11-30 21:53:58 -0500

    get tests to pass reliably

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use zero2prod::startup::run;
+use sqlx::{Connection, PgPool};
+use zero2prod::{configuration::get_configuration, startup::run};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
-    run(listener)?.await
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(
+        &configuration.database.connection_string()
+        )
+        .await
+        .expect("Failed to connect to Postgres.");
+    // getting port from settings
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener: TcpListener = TcpListener::bind(address).expect("Failed to bind to random port");
+    run(listener, connection_pool)?.await
 }
