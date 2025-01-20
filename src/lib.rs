a61098f9f17ea892c0e637a2e71f2b0b7ee0b636
joshrutta <joshrutta7@gmail.com>
2025-01-19 23:51:04 -0500

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod routes;
 pub mod startup;
 pub mod configuration;
 pub mod telemetry;
+pub mod domain;
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,4 @@
-use reqwest;
+use reqwest::{self, Client};
 use secrecy::ExposeSecret;
 use std::net::TcpListener;
 use zero2prod::{configuration::DatabaseSettings, startup::run, telemetry::{get_subscriber, init_subscriber}};
@@ -103,6 +103,36 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             response.status().as_u16(),
             "The API did not fail with 400 Bad Request when the payload was {}",
             error_message
+        );
+    }
+}
+
+#[tokio::test]
+async fn subcribe_returns_a_400_when_fields_are_present_but_empty() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        // Act
+        let response = client 
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+            // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not a return a 400 Bad Request when the payload was {}.",
+            description
         );
     }
 }
